537bb2985e567ee15396b9b5a2cdf734beaa55a3
BANDA-KING <b.k.boukers@gmail.com>
2025-03-24 17:12:15 +0100

    Update KING.py

--- KING.py
+++ KING.py
@@ -1,20 +1,4 @@
-import os
-import platform
-import time
-os.system("pip install requests")
-fuck = platform.architecture()[0]
-if fuck == '64bit':
-    os.system('git pull')
-    os.system('clear')
-    print('\x1b[1;94m[√] \x1b[1;92mYour Device is 64 bit')
-    time.sleep(2)
-    from KING import KING
-    KING()
-if fuck == '32bit':
-    os.system('git pull')
-    os.system('clear')
-    print('\x1b[1;94m[√] \x1b[1;92mYour Device is 32 bit')
-    time.sleep(2)
-    KING() 
-os.system('clear')
-print('\x1b[1;97m Soon Your Device Supported Tools ')
+import KING
+os.system("print")
+os.system('xdg-open https://chat.whatsapp.com/ImXknPvyvU9IFBballhWgy')
+import KING
